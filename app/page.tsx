b1b2f38114b9ce7c5f9b5616c5f00ae7b695f377
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-26 20:07:45 +0000

    Add subtitle text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import SearchBar from "@/components/SearchBar";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between bg-blue-500 text-white p-12">
-      <h1 className="text-4xl font-bold mb-6">Open weather visualizer</h1>
+      <h1 className="text-4xl font-bold mb-2">Open weather visualizer</h1>
+      <p className="text-lg text-gray-200 mb-8">Visualize weather data from the Open Meteo API.</p>
       <SearchBar />
     </div>
   );

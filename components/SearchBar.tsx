26d463430c7303d69721168edf1c75a70d52b078
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-26 20:07:59 +0000

    Add search bar api search function

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,48 @@
+"use client"
+import { FormEvent, useState } from 'react';
+
 export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(searchQuery)}`);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Failed to fetch geocoding api');
+        //TODO: Show error notification in the UI
+      }
+      if (!data.results) {
+        throw new Error('No results list found');
+      }
+      if (data.results.length === 0) {
+        throw new Error('No results found for search query: ' + searchQuery);
+      }
+      console.log('Search results:', data);
+      //TODO: Show results in the UI
+    } catch (error) {
+      console.error('Error searching for location:', error);
+    }
+  };
+
   return (
-    <form className="flex items-center w-full max-w-md">   
+    <form onSubmit={handleSubmit} className="flex items-center w-full max-w-md">   
         <label htmlFor="city-search" className="sr-only">Search</label>
         <div className="relative w-full">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">                
                 <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                     <path fillRule="evenodd" d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z" clipRule="evenodd"/>
                 </svg>
-
             </div>
             <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-                type="text" id="city-search"
-                placeholder="Search for a city" required
+                type="text"
+                id="city-search"
+                placeholder="Search for a city..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                required
             />
         </div>
         <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300">

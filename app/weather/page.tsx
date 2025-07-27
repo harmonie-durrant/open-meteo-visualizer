d4e728b05056d8a4fb977a09fcd5013a54460b4c
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-27 08:36:39 +0000

    Fix weekday names I found out that Date.toLocaleDateString() has options to get just the name of the weekday.

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -61,31 +61,34 @@ export default function Search() {
                 <div className="flow-root">
                   <ul role="list" className="divide-y divide-gray-200">
                     {
-                      weatherResults.daily.weather_code.map((code, index) => (
-                        <li key={index} className="py-3 sm:py-4">
-                          <div className="flex items-center gap-2">
-                            <div className="shrink-0">
-                              <p>{`${index == 0 ? "Today" : index == 1 ? "Tomorrow" : `Day +${index + 1}`}`}</p>
-                              <p>{`${new Date(Date.now() + index * 86400000).toLocaleDateString()}`}</p>
+                      weatherResults.daily.weather_code.map((code, index) => {
+                        const date = new Date(Date.now() + index * 86400000);
+                        return (
+                          <li key={index} className="py-3 sm:py-4">
+                            <div className="flex items-center gap-2">
+                              <div className="shrink-0">
+                                <p>{`${index == 0 ? "Today" : index == 1 ? "Tomorrow" : date.toLocaleDateString("en", { weekday: 'long' })}`}</p>
+                                <p>{`${date.toLocaleDateString()}`}</p>
+                              </div>
+                              <div className="flex flex-1 items-center justify-start min-w-0 gap-2">
+                                <p className="text-3xl">
+                                  {openWeatherWMOToEmoji(code).value}
+                                </p>
+                                <p className="text-md">
+                                  {openWeatherWMOToEmoji(code).description}
+                                </p>
+                              </div>
+                              <div className="inline-flex items-center gap-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-droplet" viewBox="0 0 16 16">
+                                  <path fillRule="evenodd" d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+                                  <path fillRule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
+                                </svg>
+                                <span className="font-bold">{weatherResults.daily.precipitation_probability_max[index]}%</span>
+                              </div>
                             </div>
-                            <div className="flex flex-1 items-center justify-start min-w-0 gap-2">
-                              <p className="text-3xl">
-                                {openWeatherWMOToEmoji(code).value}
-                              </p>
-                              <p className="text-md">
-                                {openWeatherWMOToEmoji(code).description}
-                              </p>
-                            </div>
-                            <div className="inline-flex items-center gap-1">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-droplet" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
-                                <path fillRule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
-                              </svg>
-                              <span className="font-bold">{weatherResults.daily.precipitation_probability_max[index]}%</span>
-                            </div>
-                          </div>
-                        </li>
-                      ))
+                          </li>
+                        )
+                      })
                     }
                   </ul>
                 </div>

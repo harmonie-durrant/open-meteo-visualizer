93f0afa4605d1eab793b70e73455e2697934be11
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-07-27 18:20:56 +0000

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SearchBar from "@/components/SearchBar";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-blue-500 text-white">
-      <h1 className="text-4xl font-bold mb-3 text-center">Open weather visualizer</h1>
+      <h1 className="text-4xl font-bold mb-3 text-center">Open météo visualizer</h1>
       <p className="text-lg text-gray-200 mb-10 text-center">Visualize weather data from the Open Meteo API.</p>
       <SearchBar />
     </div>
